734229d12265bb894a8d770fed92da79528f062d
Milen03 <milen.atanasovv03@gmail.com>
2025-09-06 18:51:58 +0300

    ужас

--- app/Http/Controllers/LaptopController.php
+++ app/Http/Controllers/LaptopController.php
@@ -21,7 +21,7 @@ class LaptopController extends Controller
      */
     public function create()
     {
-        //
+        return view('laptops.create');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\LaptopController;
@@ -8,6 +9,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth'])->group(function () {
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
     Route::resource('laptops', LaptopController::class);
 });
+
+require __DIR__.'/auth.php';
