b6b7e353074da3634605cf74e08623b2b614302d
Milen03 <milen.atanasovv03@gmail.com>
2025-09-06 18:01:48 +0300

    Add router Laptop

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\LaptopController;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('laptops', LaptopController::class);
 });
