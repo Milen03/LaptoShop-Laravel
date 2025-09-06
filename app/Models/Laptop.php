229fccc770548390a2cf631cf030d74453ef70e2
Milen03 <milen.atanasovv03@gmail.com>
2025-09-06 16:43:39 +0300

    Make relacion

--- app/Models/Laptop.php
+++ app/Models/Laptop.php
@@ -4,7 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\User;
+
 class Laptop extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'brand',
+        'model',
+        'year',
+        'description',
+        'image',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
