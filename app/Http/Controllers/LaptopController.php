c9d6dcd0bc7ffcda89bc846a42bd69c5d517c670
Milen03 <milen.atanasovv03@gmail.com>
2025-09-06 18:09:30 +0300

    Make index

--- app/Http/Controllers/LaptopController.php
+++ app/Http/Controllers/LaptopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Laptop;
 use Illuminate\Http\Request;
 
 class LaptopController extends Controller
@@ -11,7 +12,8 @@ class LaptopController extends Controller
      */
     public function index()
     {
-        
+        $laptops = Laptop::latest()->get();
+        return view('laptops.index', compact('laptops'));
     }
 
     /**
